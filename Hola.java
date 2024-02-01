650e3f9920e8feab89cb9db0958061a0b8f642ba
ppsprofe <134409342+ppsprofe@users.noreply.github.com>
2024-02-01 22:23:46 +0100

    multiplicacio i's

--- Hola.java
+++ Hola.java
@@ -1,10 +1,12 @@
 public class Hola{
    public static void main (String[] args){
 
-	System.out.println("Hola DAW");
-	System.out.println("Adeu");
-        for (int i=0; i<5; i++){
-           System.out.println(i);
-        }
+      System.out.println("Hola DAW");
+      System.out.println("Adeu");
+      for (int i=0; i<5; i++){
+         System.out.println(i*i);
+      }
+
+
    }
 }
